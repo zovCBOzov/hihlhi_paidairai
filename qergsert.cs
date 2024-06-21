3bd9a0aff9cb2404c9cfbe113bcf4b2418f30de5
zovCBOzov <alehsa2009@gmail.com>
2024-06-21 15:09:34 +0300

    Add files via upload

---
+++ qergsert.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class qergsert : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Print(HelloWorld)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
